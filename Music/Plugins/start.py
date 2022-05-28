d56f5caae56219813cfd2b1c2ea2306119ead663
zeinzo <91390078+zeinzo@users.noreply.github.com>
2022-05-28 23:42:01 +0700

    Update start.py

--- Music/Plugins/start.py
+++ Music/Plugins/start.py
@@ -130,6 +130,7 @@ async def play(_, message: Message):
 """,
             parse_mode="markdown",
             reply_markup=pstart_markup,
+            disable_web_page_preview=True,
             reply_to_message_id=message.message_id,
         )
     elif len(message.command) == 2:

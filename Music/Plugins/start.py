18039a7ea740feca95be64dc98f8cd1c4fcd8f3c
CuteNeehh <105994116+Neehh@users.noreply.github.com>
2022-06-03 06:18:33 +0700

    Update start.py

--- Music/Plugins/start.py
+++ Music/Plugins/start.py
@@ -25,11 +25,11 @@ from pyrogram.types import (
 def start_pannel():
     buttons = [
         [
-            InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/gatchornihbos"),
-            InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url=f"https://t.me/diarygatchor"),
+            InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/hiroosupport"),
+            InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url=f"https://t.me/hiroshimabes"),
         ],
         [
-            InlineKeyboardButton("sᴛʀɪɴɢ sᴇssɪᴏɴ ʙᴏᴛ", url=f"https://t.me/Hirostringbot"),
+            InlineKeyboardButton("ᴄᴏᴍᴍᴀɴᴅ", url=f"https://telegra.ph/Turbo-05-28""),
         ],
     ]
     return (
@@ -45,12 +45,12 @@ pstart_markup = InlineKeyboardMarkup(
                 "÷ ᴛᴀᴍʙᴀʜᴋᴀɴ sᴀʏᴀ ᴋᴇ ɢʀᴏᴜᴘ ᴀɴᴅᴀ ÷", url=f"https://t.me/{BOT_USERNAME}?startgroup=true"),
         ],
         [
-            InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/gatchornihbos"),
-            InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url=f"https://t.me/diarygatchor"),
+            InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/hiroosupport"),
+            InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url=f"https://t.me/hiroshimabes"),
         ],
         [
             InlineKeyboardButton( 
-                "ᴅᴇᴠᴇʟᴏᴘᴇʀ", url=f"https://t.me/pnyajoss"),
+                "ᴅᴇᴠᴇʟᴏᴘᴇʀ", url=f"https://t.me/xcarfaceall"),
             InlineKeyboardButton("ᴄᴏᴍᴍᴀɴᴅ", url="https://telegra.ph/Turbo-05-28"),
         ],
         [

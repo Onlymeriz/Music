9f6a56ac19c085abee498fbc976d50da9add2322
khansa gunawan <112273130+failedc0de@users.noreply.github.com>
2023-01-20 17:15:19 +0700

    Update config.py

--- Music/config.py
+++ Music/config.py
@@ -3,7 +3,8 @@
 from os import getenv
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv("config.env")
+
 get_queue = {}
 SESSION_NAME = getenv('SESSION_NAME', 'session')
 BOT_TOKEN = getenv('BOT_TOKEN')

2f984face61db3c1e172b4ec8f890d886b8db6e8
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-02-04 16:34:22 +0700

    Update config.py

--- Music/config.py
+++ Music/config.py
@@ -3,14 +3,14 @@
 from os import getenv
 from dotenv import load_dotenv
 
-load_dotenv("config.env")
+load_dotenv()
 
 get_queue = {}
 SESSION_NAME = getenv('SESSION_NAME', 'session')
 BOT_TOKEN = getenv('BOT_TOKEN')
-API_ID = int(getenv('API_ID', "10892147"))
+API_ID = int(getenv('API_ID', "1054295664"))
 API_HASH = getenv('API_HASH')
-DURATION_LIMIT = int(getenv('DURATION_LIMIT', '50000'))
+DURATION_LIMIT = int(getenv('DURATION_LIMIT', "5000000"))
 COMMAND_PREFIXES = list(getenv('COMMAND_PREFIXES', '/ . , : ; !').split())
 MONGO_DB_URI = getenv("MONGO_DB_URI")
 SUDO_USERS = list(map(int, getenv('SUDO_USERS', '').split()))
